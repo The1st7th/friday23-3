652f5c3ce28f21bd825243748d04127105d006c8
emmanuel nicolas <eric.e.nicolas@gmail.com>
2018-03-23 15:04:28 -0700

    added name and sroll to bottom page

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,41 @@
 $(document).ready(function() {
+$("body").fadeIn("slow");
   $("#validation").click(function() {
 var answer1 = $("input:radio[name=1st]:checked").val();
 var answer2 = $("input:radio[name=2nd]:checked").val();
 var answer3 = $("input:radio[name=3rd]:checked").val();
 var answer4 = $("input:radio[name=4th]:checked").val();
 var answer5 = $("input:radio[name=5th]:checked").val();
-console.log(answer1);
-console.log(answer2);
-console.log(answer3);
-console.log(answer4);
-console.log(answer5);
+var name = $("textarea").val();
+$("p span").text(name +" ");
 if (answer1 === "yes"){
 $(".language").hide();
-console.log("entered1");
-$("#c").show();   
+$("#c").fadeIn(1000);
+window.scrollTo(2000,document.body.scrollHeight);
 }
 else{
         if(answer2 === "yes"){
         $(".language").hide();
-        console.log("entered2");
-        $("#c").show(); 
+        $("#c").fadeIn(1000);
+        window.scrollTo(1010,document.body.scrollHeight);
         } 
         else{
             if(answer3 === "yes"){
             $(".language").hide();
-            console.log("entered3");
-            $("#c").show();     
+            $("#c").fadeIn(1000);
+            window.scrollTo(1010,document.body.scrollHeight);
         }
             else{
                 if(answer4 == "yes"){
                     $(".language").hide();
-                    console.log("entered4");
-                    $("#j").show(); 
+                    $("#j").fadeIn(1000);
+                    window.scrollTo(2000,document.body.scrollHeight);
                 }
                 else{
                     if(answer5 == "yes" || answer5 =="no"){
                     $(".language").hide();
-                    console.log("entered5");
-                    $("#js").show();
+                    $("div#js").fadeIn(1000);
+                    window.scrollTo(1010,document.body.scrollHeight);
                     }
                 }
             }
